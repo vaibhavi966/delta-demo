39102e0bd3e2fbcf621d18850a9e8b1665c3968a
Vaibhavi966 <vaibhavijha10@gmail.com>
2026-08-09 19:59:55 +0530

    fix homepage redirect

--- app.js
+++ app.js
@@ -95,7 +95,7 @@ app.use((req,res,next)=>{
 
   res.locals.success=req.flash("success");
   res.locals.error=req.flash("error");
-  res.locals.currUser=req.user;
+  res.locals.currUser=req.user || null;
   next();
 });
 
@@ -113,10 +113,14 @@ app.use((req,res,next)=>{
 });
 
 //middlewares error
-app.use((err,req,res,next)=>{
-  let{statusCode=500,message="something went wrong"}=err; 
-  res.render("error.ejs",{message});
-  
+app.use((err, req, res, next) => {
+    console.log("ACTUAL ERROR:", err);
+    console.log("ERROR MESSAGE:", err.message);
+    console.log("ERROR STACK:", err.stack);
+
+    let { statusCode = 500, message = "something went wrong" } = err;
+
+    res.status(statusCode).render("error.ejs", { message });
 });
 
 
