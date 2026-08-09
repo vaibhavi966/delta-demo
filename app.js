86304520c5c90a00f73910924eed480e50c3b479
Vaibhavi966 <vaibhavijha10@gmail.com>
2026-08-09 20:22:42 +0530

    fix render server and root route

--- app.js
+++ app.js
@@ -124,6 +124,8 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(8080,()=>{
-    console.log("server is working");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
